d247a0c7eaf0048b58617bdfbf7719a51fa8bdee
petrcichra <103201475+petrcichra@users.noreply.github.com>
2022-05-31 20:22:40 +0200

    Vytvoření nové branche s testem, který hodí chybu

--- src/main/java/org/mvn_pull_req/Main.java
+++ src/main/java/org/mvn_pull_req/Main.java
@@ -2,6 +2,13 @@ package org.mvn_pull_req;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(sectiCisla(1,1));
+
+    }
+
+    // test metody
+    public static int sectiCisla(int a, int b) {
+        return (a+b);
     }
 }
